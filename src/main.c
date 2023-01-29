9ba5289cffae89c122629c4265c70fe82ca80c42
ajudamee <93143232+ajudamee@users.noreply.github.com>
2023-01-29 18:50:44 +0000

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,26 +1,16 @@
 /**
-  ******************************************************************************
-  * @file    main.c
-  * @author  3S0 FreeRTOs
-  * @version V1.0
-  * @date    24/10/2017
-  * @brief   FreeRTOS Example project.
-  ******************************************************************************
+  1- sensor Temperatura --> Ok
+  2- sensor Pressão
+  3- tick timer
+  4- desenhar copo
+  5- criar unica funçao do lcd LCD TASK
 */
-
-
-/*
- *
- * Messages Queues
- * 2017-2018
- *
- */
-
 /* Standard includes. */
 #include <string.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <lcd.h>
+#include <stm32f10x.h>
 
 /* Scheduler includes. */
 #include "FreeRTOS.h"
@@ -44,33 +34,26 @@ static void prvSetupRCC( void );
 /* Configure GPIO. */
 static void prvSetupGPIO( void );
 
-static void I2C2_Config(void);
-
-static I2C_write(uint8_t dev_addr, uint8_t reg_addr, uint8_t reg_data);
-
-static uint8_t I2C_read(uint8_t dev_addr, uint8_t reg_addr);
-
-/* Configure the ADC */
-static void prvSetupADC( void );
-
 /* Simple LED toggle task. */
 static void prvFlashTask1( void *pvParameters );
 
 /* LCD activity task. */
 static void prvLcdTask( void *pvParameters );
+/* ADC temperature read task. */
+static void prvTempTask( void *pvParameters );
 
-/* Configure the ADC */
-static void prvSetupADC( void );
+static void prvTask4( void *pvParameters );
 
+static void prvTempTask3(void *pvParameters);
 /* Button task. */
 static void prvButtonTask( void *pvParameters );
 
-static void prvTempTask(void *pvParameters);
-
-static void prvTask4(void *pvParameters);
-
 static void prvUSART2Interrupt ( void );
 static void prvSetupEXTI1( void );
+static void prvSetupEXTI15_10(void);
+
+/* Configure the ADC */
+static void prvSetupADC( void );
 
 /********** Useful functions **********/
 /* USART2 configuration. */
@@ -80,8 +63,6 @@ static void prvSetupUSART2( void );
 static void prvSendMessageUSART2(char *message);
 
 /***************************************/
-
-
 /* Task 1 handle variable. */
 TaskHandle_t HandleTask1;
 
@@ -94,20 +75,39 @@ TaskHandle_t HandleTask3;
 /* Task 4 handle variable. */
 TaskHandle_t HandleTask4;
 
+/* Task 5 handle variable. */
 TaskHandle_t HandleTask5;
 
+/* Task 5 handle variable. */
 TaskHandle_t HandleTask6;
 
 /*Create a Queue*/
 QueueHandle_t xQueue;  /* Global variable. */
 
-//Ex.2
 QueueHandle_t xQueue1;  /* Global variable. */
 
-//Ex.3
 QueueHandle_t xQueue2;  /* Global variable. */
 
+QueueHandle_t button_queue;  /* Global variable. */
+
 QueueHandle_t xQueue3;
+
+#define DEBOUNCE_TIME_MS 100
+
+typedef struct button_t {
+    uint16_t gpio_pin;
+    GPIO_TypeDef* gpio_port;
+    char character;
+    uint32_t last_press_time;
+    uint8_t state;
+} button_t;
+
+//Declaração dos botões
+button_t button_PA1 = {GPIO_Pin_1, GPIOA, 'O', 0, 0};
+button_t button_PC10 = {GPIO_Pin_10, GPIOC, 'U', 0, 0};
+button_t button_PC11 = {GPIO_Pin_11, GPIOC, 'L', 0, 0};
+button_t button_PC12 = {GPIO_Pin_12, GPIOC, 'R', 0, 0};
+button_t button_PC13 = {GPIO_Pin_13, GPIOC, 'D', 0, 0};
 
 
 typedef struct val{
@@ -120,14 +120,11 @@ int main( void )
 	/*Setup the hardware, RCC, GPIO, etc...*/
     prvSetupRCC();
     prvSetupGPIO();
-    prvSetupADC();
-
-
-    I2C2_Config();
-    prvSetupADC();
     prvSetupUSART2();
     prvUSART2Interrupt();
     prvSetupEXTI1();
+    prvSetupADC();
+    prvSetupEXTI15_10();
 
     //Create a message queue with a size of 10 and element size of 1 byte
     xQueue = xQueueCreate(10, sizeof(char));
@@ -155,8 +152,19 @@ int main( void )
     		/* Queue created successfully. */
 		}
 
-    xQueue2 = xQueueCreate(20, sizeof(int32_t));
-    if( xQueue2 == 0 ) {
+    xQueue2 = xQueueCreate(20, sizeof(char));
+        if( xQueue2 == 0 ) {
+
+           	/* Queue was not created and must not be used. */
+
+           	}else
+       		{
+           		/* Queue created successfully. */
+       		}
+
+
+    button_queue = xQueueCreate(20, sizeof(char));
+    if( button_queue == 0 ) {
 
        	/* Queue was not created and must not be used. */
 
@@ -178,19 +186,46 @@ int main( void )
 	/* Create the tasks */
  	xTaskCreate( prvLcdTask, "Lcd", configMINIMAL_STACK_SIZE, NULL, mainLCD_TASK_PRIORITY, &HandleTask1 );
  	xTaskCreate( prvFlashTask1, "Flash1", configMINIMAL_STACK_SIZE, NULL, mainFLASH_TASK_PRIORITY, &HandleTask2 );
-    xTaskCreate( prvTempTask, "Temp", configMINIMAL_STACK_SIZE+100, NULL, mainFLASH_TASK_PRIORITY, &HandleTask3 );
-    xTaskCreate( prvButtonTask, "Button",configMINIMAL_STACK_SIZE+100, NULL, mainFLASH_TASK_PRIORITY, &HandleTask4 );
-
-    xTaskCreate( prvTask4, "Usart", configMINIMAL_STACK_SIZE, NULL, mainFLASH_TASK_PRIORITY, &HandleTask6 );
+    xTaskCreate( prvButtonTask, "Button",configMINIMAL_STACK_SIZE+100, NULL, mainFLASH_TASK_PRIORITY, &HandleTask3 );
+    xTaskCreate( prvTempTask, "Temp", configMINIMAL_STACK_SIZE+100, NULL, mainFLASH_TASK_PRIORITY, &HandleTask4 );
+    xTaskCreate( prvTempTask3, "Temp Display", configMINIMAL_STACK_SIZE, NULL, mainFLASH_TASK_PRIORITY, &HandleTask5 );
+    xTaskCreate( prvTask4, "Temp task", configMINIMAL_STACK_SIZE, NULL, mainFLASH_TASK_PRIORITY, &HandleTask6 );
     /* Start the scheduler. */
 	vTaskStartScheduler();
 
 	/* Will only get here if there was not enough heap space to create the idle task. */
 	return 0;
 }
+
 /*-----------------------------------------------------------*/
 
 
+static void prvSetupADC( void )
+{
+    ADC_InitTypeDef ADC_InitStructure;
+
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
+
+    ADC_InitStructure.ADC_Mode = ADC_Mode_Independent;
+    ADC_InitStructure.ADC_ScanConvMode = DISABLE;
+    ADC_InitStructure.ADC_ContinuousConvMode = DISABLE;
+    ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
+    ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+    ADC_InitStructure.ADC_NbrOfChannel = 1;
+    ADC_Init(ADC1, &ADC_InitStructure);
+
+    ADC_TempSensorVrefintCmd(ENABLE);
+    ADC_RegularChannelConfig(ADC1, ADC_Channel_16,1, ADC_SampleTime_239Cycles5);
+    ADC_Cmd(ADC1, ENABLE);
+
+    ADC_ResetCalibration(ADC1);
+    while(ADC_GetResetCalibrationStatus(ADC1));
+    ADC_StartCalibration(ADC1);
+    while(ADC_GetCalibrationStatus(ADC1));
+
+}
+
+/*-----------------------------------------------------------*/
 static void prvFlashTask1( void *pvParameters )
 {
     TickType_t xLastExecutionTime;
@@ -207,21 +242,24 @@ static void prvFlashTask1( void *pvParameters )
 /*-----------------------------------------------------------*/
 
 
-/* Example task to present characters in ther display. */
+/* Example task to present characters in ther display.*/
+
 static void prvLcdTask( void *pvParameters )
 {
 	lcd_init( );
-	char ulVar;
-
+	char buf[30];
+	static uint32_t start_time = 0;
 	for(;;)
 	{
-		xQueueReceive(xQueue, &ulVar, (TickType_t) portMAX_DELAY );
-		lcd_draw_char( 63-(5*10)/2, 79-(7*10)/2, ulVar, 0xFFFF, 10 );
+		// progress bar //
+		for(int i = 10; i <= 140; i += 10) lcd_draw_rect(10, i, 30, 10, 0xFFFF);
+
+		//xQueueReceive(xQueue, &ulVar, (TickType_t) portMAX_DELAY );
+		//lcd_draw_char( 63-(5*10)/2, 79-(7*10)/2, ulVar, 0xFFFF, 10 );
 		vTaskDelay( (TickType_t ) 2000 / portTICK_RATE_MS);
 	}
 }
 /*-----------------------------------------------------------*/
-
 
 /* Temperature task - demo to read the ADC and get the temperature. */
 /* Change this task accordingly. */
@@ -230,9 +268,8 @@ static void prvTempTask( void *pvParameters )
     TickType_t xLastExecutionTime;
     int ADC1ConvertedValue=0;
     int32_t temp;
-    char buf[30];
 
-    TickType_t tick;
+    val valores;
 
     xLastExecutionTime = xTaskGetTickCount();
     for( ;; )
@@ -254,16 +291,36 @@ static void prvTempTask( void *pvParameters )
 
         //Ex.3
 
-        tick= xTaskGetTickCount();
 
-    	sprintf(buf, "%ld %ld\r\n", temp, tick); //todo apresentar no lcd
+        xQueueSendToBack(xQueue2, (void *) &temp, (TickType_t) 10);
 
-    	prvSendMessageUSART2(buf);
+
+        valores.temp = temp;
+        valores.tick = xTaskGetTickCount();
+
+        xQueueSendToBack(xQueue3, (void *) &valores, (TickType_t) 10);
 
     }
 }
 /*-----------------------------------------------------------*/
 
+static void prvTempTask3(void *pvParameters){
+	lcd_init();
+	int32_t temp;
+	TickType_t t1;
+	char buf_temp[20];
+	char buf_tick[20];
+	for(;;){
+		xQueueReceive(xQueue2, &temp, (TickType_t) portMAX_DELAY);
+		sprintf(buf_temp, "Temp: %ld", temp);
+		lcd_draw_string(60,20,buf_temp, 0xFFFF, 1);
+		//sprintf(buf_tick, "Tick: %d", t1);
+		//lcd_draw_string(10,20,buf_tick, 0xFFFF, 1);
+
+	}
+}
+
+/*-----------------------------------------------------------*/
 static void prvTask4(void *pvParameters){
 	lcd_init();
 	char buf[30];
@@ -272,13 +329,12 @@ static void prvTask4(void *pvParameters){
 
 	xQueueReceive(xQueue3, &valores, (TickType_t) portMAX_DELAY);
 	sprintf(buf, "%ld %ld\r\n", valores.temp, valores.tick);
-
 	prvSendMessageUSART2(buf);
 
 	}
 }
 
-
+/*-----------------------------------------------------------*/
 static void prvButtonTask( void *pvParameters ){
 	//lcd_init( );
 	//static BaseType_t pxHigherPriorityTaskWoken;
@@ -286,32 +342,27 @@ static void prvButtonTask( void *pvParameters ){
 	TickType_t t1, t2 =0 , dif;
 
 	char buf[30];
-	for(;;){
+	char buf_tick[60];
+    char button_char;
 
-		//xQueueReceiveFromISR(xQueue1, &t1, pxHigherPriorityTaskWoken);
-		xQueueReceive(xQueue1, &t1, (TickType_t) portMAX_DELAY);
-		//GPIO_WriteBit(GPIOB, GPIO_Pin_1, (1-GPIO_ReadOutputDataBit(GPIOB, GPIO_Pin_1)));
-		dif= (t1-t2);
-		//sprintf(buf, "1: %ld ,  2: %ld %ld\r\n", t1, t2, dif);
+    for(;;){
 
-		t2=t1;
+
+
+    	dif= (t1-t2);
+
+		xQueueReceive(button_queue, (void *) &button_char, portMAX_DELAY);
+		sprintf(buf, "1: %ld ,  2: %ld %ld\r\n", t1, t2, dif);
+		//t2=t1;
 		prvSendMessageUSART2(buf);
 
-
-
-		if(dif < 800){
-			lcd_draw_char( 63-(5*10)/2, 79-(7*10)/2, '2', 0xFFFF, 10 );
-			//lcd_draw_char( 0, 0, buf, 0xFFFF, 1 );
-			//prvSendMessageUSART2(buf);
+		GPIO_WriteBit(GPIOB, GPIO_Pin_1, (1-GPIO_ReadOutputDataBit(GPIOB, GPIO_Pin_1)));
+		if(dif < 100){
+			//lcd_draw_char( 63-(5*10)/2, 79-(7*10)/2, button_char, 0xFFFF, 1 );
+			lcd_draw_char( 60, 10, button_char, 0xFFFF, 1 );
 		}
-		else{
-			lcd_draw_char( 63-(5*10)/2, 79-(7*10)/2, '1', 0xFFFF, 10 );
-		}
-		/*
-		while(USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET);
-					USART_SendData(USART2, t1);
-		USART_ClearFlag(USART2,USART_FLAG_TXE);*/
-	}
+    }
+    return button_char;
 }
 
 static void prvSetupRCC( void )
@@ -363,23 +414,7 @@ static void prvSetupGPIO( void )
 {
     /* GPIO configuration - GREEN LED*/
     GPIO_InitTypeDef GPIO_InitStructure;
-
-
-
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C2, ENABLE);
-
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
-
-
-
-	//Inicialização I2C
-	GPIO_InitTypeDef GPIO_InitTypeDefStruct;
-	GPIO_StructInit(&GPIO_InitTypeDefStruct);
-	GPIO_InitTypeDefStruct.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11; // SCL, SDA, SA0
-	GPIO_InitTypeDefStruct.GPIO_Mode = GPIO_Mode_AF_OD;
-	GPIO_InitTypeDefStruct.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOB, &GPIO_InitTypeDefStruct);
-
+    RCC_APB2PeriphClockCmd( RCC_APB2Periph_AFIO , ENABLE );
 
     /* Enable GPIOB clock */
     RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOB , ENABLE );
@@ -387,7 +422,10 @@ static void prvSetupGPIO( void )
     /* Enable GPIOA clock */
     RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOA , ENABLE );
 
-    GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_9;
+    /* Enable GPIOC clock */
+    RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOC , ENABLE );
+
+    GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_0 | GPIO_Pin_1 ;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 
@@ -398,101 +436,16 @@ static void prvSetupGPIO( void )
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11 | GPIO_Pin_12 | GPIO_Pin_13;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
 }
 /*-----------------------------------------------------------*/
 
-void I2C2_Config(void)
-{
-	//Configuração I2C
 
-	I2C_InitTypeDef I2C_InitStructure;
-
-	I2C_StructInit(&I2C_InitStructure);
-	I2C_InitStructure.I2C_Mode = I2C_Mode_I2C;
-	I2C_InitStructure.I2C_ClockSpeed = 400000;
-	I2C_InitStructure.I2C_DutyCycle = I2C_DutyCycle_2;
-	I2C_InitStructure.I2C_OwnAddress1 = 0x00;
-	I2C_InitStructure.I2C_Ack = I2C_Ack_Enable;
-	I2C_InitStructure.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
-	I2C_Init(I2C2, &I2C_InitStructure);
-	I2C_Cmd(I2C2, ENABLE);
-
-}
-
-void I2C_write(uint8_t dev_addr, uint8_t reg_addr, uint8_t reg_data)
-{
-	while(!I2C_GetFlagStatus(I2C2, I2C_FLAG_BUSY)); // while the bus is busy
-
-	I2C_AcknowledgeConfig(I2C2, ENABLE);
-
-	I2C_GenerateSTART(I2C2, ENABLE); // send I2C start condition
-	while(!I2C_CheckEvent(I2C2, I2C_EVENT_MASTER_MODE_SELECT));
-
-	I2C_Send7bitAddress(I2C2, dev_addr, I2C_Direction_Transmitter); // send device slave address for write
-	while(!I2C_CheckEvent(I2C2, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
-
-	I2C_SendData(I2C2, reg_addr); // send the device internal register address
-	while(!I2C_CheckEvent(I2C2, I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-
-	I2C_SendData(I2C2, reg_data); // send data to register
-	while(!I2C_CheckEvent(I2C2, I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-
-	I2C_GenerateSTOP(I2C2, ENABLE);
-}
-
-uint8_t I2C_read(uint8_t dev_addr, uint8_t reg_addr)
-{
-	I2C_AcknowledgeConfig(I2C2, DISABLE);
-	while(I2C_GetFlagStatus(I2C2, I2C_FLAG_BUSY)); // while the bus is busy
-
-	I2C_GenerateSTART(I2C2, ENABLE); // send I2C start condition
-	while(!I2C_CheckEvent(I2C2, I2C_EVENT_MASTER_MODE_SELECT));
-
-	I2C_Send7bitAddress(I2C2, dev_addr, I2C_Direction_Transmitter); // send device slave address for write
-	while(!I2C_CheckEvent(I2C2, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
-
-	I2C_SendData(I2C2, reg_addr); //0x80 | reg_addr); // send the device internal register address
-	while(!I2C_CheckEvent(I2C2, I2C_EVENT_MASTER_BYTE_TRANSMITTED));
-
-	I2C_GenerateSTART(I2C2, ENABLE); // repeated start
-	while(!I2C_CheckEvent(I2C2, I2C_EVENT_MASTER_MODE_SELECT));
-
-	I2C_Send7bitAddress(I2C2, dev_addr, I2C_Direction_Receiver); // send device slave address for read
-	while(!I2C_CheckEvent(I2C2, I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED));
-	while(!I2C_CheckEvent(I2C2, I2C_EVENT_MASTER_BYTE_RECEIVED));
-
-	uint8_t read_reg = I2C_ReceiveData(I2C2);
-
-	I2C_GenerateSTOP(I2C2, ENABLE);
-	I2C_AcknowledgeConfig(I2C2, DISABLE);
-
-	return read_reg;
-}
-
-static void prvSetupADC( void )
-{
-    ADC_InitTypeDef ADC_InitStructure;
-
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
-
-    ADC_InitStructure.ADC_Mode = ADC_Mode_Independent;
-    ADC_InitStructure.ADC_ScanConvMode = DISABLE;
-    ADC_InitStructure.ADC_ContinuousConvMode = DISABLE;
-    ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
-    ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
-    ADC_InitStructure.ADC_NbrOfChannel = 1;
-    ADC_Init(ADC1, &ADC_InitStructure);
-
-    ADC_TempSensorVrefintCmd(ENABLE);
-    ADC_RegularChannelConfig(ADC1, ADC_Channel_16,1, ADC_SampleTime_239Cycles5);
-    ADC_Cmd(ADC1, ENABLE);
-
-    ADC_ResetCalibration(ADC1);
-    while(ADC_GetResetCalibrationStatus(ADC1));
-    ADC_StartCalibration(ADC1);
-    while(ADC_GetCalibrationStatus(ADC1));
-
-}
 
 void prvSetupUSART2( void )
 {
@@ -556,33 +509,6 @@ uint16_t cont_aux=0;
 }
 /*-----------------------------------------------------------*/
 
-
-static void prvSetupADC( void )
-{
-    ADC_InitTypeDef ADC_InitStructure;
-
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
-
-    ADC_InitStructure.ADC_Mode = ADC_Mode_Independent;
-    ADC_InitStructure.ADC_ScanConvMode = DISABLE;
-    ADC_InitStructure.ADC_ContinuousConvMode = DISABLE;
-    ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
-    ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
-    ADC_InitStructure.ADC_NbrOfChannel = 1;
-    ADC_Init(ADC1, &ADC_InitStructure);
-
-    ADC_TempSensorVrefintCmd(ENABLE);
-    ADC_RegularChannelConfig(ADC1, ADC_Channel_16,1, ADC_SampleTime_239Cycles5);
-    ADC_Cmd(ADC1, ENABLE);
-
-    ADC_ResetCalibration(ADC1);
-    while(ADC_GetResetCalibrationStatus(ADC1));
-    ADC_StartCalibration(ADC1);
-    while(ADC_GetCalibrationStatus(ADC1));
-
-}
-/*-----------------------------------------------------------*/
-
 static void prvUSART2Interrupt ( void )
 {
 	NVIC_InitTypeDef NVIC_InitStructure;
@@ -601,6 +527,7 @@ static void prvUSART2Interrupt ( void )
 
 static void prvSetupEXTI1( void )
 {
+
     /*NVIC configuration*/
     NVIC_InitTypeDef NVIC_InitStructure;
 
@@ -620,6 +547,34 @@ static void prvSetupEXTI1( void )
     EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising;//Rising Edge
     EXTI_InitStructure.EXTI_LineCmd = ENABLE;
     EXTI_Init(&EXTI_InitStructure);
+
+
 }
 
 
+void prvSetupEXTI15_10(void){
+
+    /*NVIC configuration*/
+    NVIC_InitTypeDef NVIC_InitStructure;
+
+    /* Enable the EXTI0 Interrupt */
+    NVIC_InitStructure.NVIC_IRQChannel = EXTI15_10_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+
+    /*Configure Key Button EXTI Line to generate an interrupt on falling edge*/
+    EXTI_InitTypeDef EXTI_InitStructure;
+
+    /*Connect Key Button EXTI Line to Key Button GPIO Pin*/
+    GPIO_EXTILineConfig(GPIO_PortSourceGPIOC, GPIO_PinSource10);
+    GPIO_EXTILineConfig(GPIO_PortSourceGPIOC, GPIO_PinSource11);
+    GPIO_EXTILineConfig(GPIO_PortSourceGPIOC, GPIO_PinSource12);
+    GPIO_EXTILineConfig(GPIO_PortSourceGPIOC, GPIO_PinSource13);
+
+    EXTI_InitStructure.EXTI_Line = EXTI_Line10 | EXTI_Line11 | EXTI_Line12 | EXTI_Line13;
+    EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
+    EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising;//Rising Edge
+    EXTI_InitStructure.EXTI_LineCmd = ENABLE;
+    EXTI_Init(&EXTI_InitStructure);
+
+}
